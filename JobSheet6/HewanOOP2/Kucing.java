bae67ee219c650cebe5873007858aa7c873aae15
Mokhammad Ilham Putra W <ilhampolpro45@gmail.com>
2025-10-02 16:37:18 +0700

    jobsheet6_Percobaan3

---
+++ JobSheet6/HewanOOP2/Kucing.java
@@ -0,0 +1,10 @@
+package JobSheet6.HewanOOP2;
+
+class Kucing extends Hewan {
+  String ras;
+  public Kucing(String nama, int umur, String ras){
+    super(nama, umur);
+    this.ras=ras;
+    System.out.println("Konstruktor Kucing dipanggil");
+  }
+}
